f77ce92034334ffbd704dc7fffa3dbde31432fb6
Stepan Tarabin <upconett@gmail.com>
2026-06-25 22:06:40 +0300

    feat: initial commit

---
+++ app/src/main/java/ru/server53/launcher/Main.java
@@ -0,0 +1,12 @@
+package ru.server53.launcher;
+
+
+public class Main {
+    protected static void printHello() {
+        System.out.println("Hello from Server53 Launcher <3");
+    }
+
+    public static void main(String[] args) {
+        printHello();
+    }
+}
